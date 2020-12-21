e9158006395d28a6d142cc80f7a02d5f26210daa
Rafael_Garcia <pmsp.smti.dev@hotmail.com>
2020-12-21 15:34:52 -0300

    Added http status

--- api/shared/utilities.php
+++ api/shared/utilities.php
@@ -1,33 +1,80 @@
 <?php
 class Utilities{
-  public function getPaging($page, $total_rows, $recordsPage, $pageURL){
-    $paging_arr = array();
+  public function getPaging($page, $totalRows, $recordsPage, $pageURL){
+    $pagingArr = array();
 
-    $paging_arr["first"] = $page > 1 ? "{$pageURL}page=1" : "";
+    $pagingArr["first"] = $page > 1 ? "{$pageURL}page=1" : "";
 
-    $totalPages = ceil($total_rows / $recordsPage);
+    $totalPages = ceil($totalRows / $recordsPage);
 
     $range = 2;
 
-    $initial_num = $page - $range;
+    $initialNum = $page - $range;
     $conditionalLimitNum = ($page+$range) + 1;
 
-    $paging_arr["pages"] = array();
+    $pagingArr["pages"] = array();
     $pageCount = 0;
 
-    for($x = $initial_num; $x < $conditionalLimitNum; $x++){
+    for($x = $initialNum; $x < $conditionalLimitNum; $x++){
       if(($x > 0) && ($x <= $totalPages)){
-        $paging_arr['pages'][$pageCount]["page"]=$x;
-          $paging_arr['pages'][$pageCount]["url"]="{$pageURL}page={$x}";
-          $paging_arr['pages'][$pageCount]["current_page"] = $x==$page ? "yes" : "no";
+        $pagingArr['pages'][$pageCount]["page"]=$x;
+          $pagingArr['pages'][$pageCount]["url"]="{$pageURL}page={$x}";
+          $pagingArr['pages'][$pageCount]["current_page"] = $x==$page ? "yes" : "no";
   
           $pageCount++;
       }
     }
 
-    $paging_arr["last"] = $page<$totalPages ? "{$pageURL}page={$totalPages}" : "";
+    $pagingArr["last"] = $page<$totalPages ? "{$pageURL}page={$totalPages}" : "";
 
-    return $paging_arr;
+    return $pagingArr;
+  }
+
+  public function getHttpStatusMessage($statusCode){
+    $statusMessages = array(
+      100 => 'Continue',  
+      101 => 'Switching Protocols',  
+      200 => 'OK',
+      201 => 'Created',  
+      202 => 'Accepted',  
+      203 => 'Non-Authoritative Information',  
+      204 => 'No Content',  
+      205 => 'Reset Content',  
+      206 => 'Partial Content',  
+      300 => 'Multiple Choices',  
+      301 => 'Moved Permanently',  
+      302 => 'Found',  
+      303 => 'See Other',  
+      304 => 'Not Modified',  
+      305 => 'Use Proxy',  
+      306 => '(Unused)',  
+      307 => 'Temporary Redirect',  
+      400 => 'Bad Request',  
+      401 => 'Unauthorized',  
+      402 => 'Payment Required',  
+      403 => 'Forbidden',  
+      404 => 'Not Found',  
+      405 => 'Method Not Allowed',  
+      406 => 'Not Acceptable',  
+      407 => 'Proxy Authentication Required',  
+      408 => 'Request Timeout',  
+      409 => 'Conflict',  
+      410 => 'Gone',  
+      411 => 'Length Required',  
+      412 => 'Precondition Failed',  
+      413 => 'Request Entity Too Large',  
+      414 => 'Request-URI Too Long',  
+      415 => 'Unsupported Media Type',  
+      416 => 'Requested Range Not Satisfiable',  
+      417 => 'Expectation Failed',  
+      500 => 'Internal Server Error',  
+      501 => 'Not Implemented',  
+      502 => 'Bad Gateway',  
+      503 => 'Service Unavailable',  
+      504 => 'Gateway Timeout',  
+      505 => 'HTTP Version Not Supported'
+    );
+    return ($statusMessages[$statusCode]) ? $statusMessages[$statusCode] : $statusMessages[500];
   }
 }
 
